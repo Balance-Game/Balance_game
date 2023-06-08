af7146576123e10610984dfe4f7d3df613c5f5a1
anys34 <anyeseong34@gmail.com>
2023-06-08 21:12:47 +0900

    ADD | login 기능 구현

--- server.js
+++ server.js
@@ -23,16 +23,37 @@ app.get("/signin", function (req, res) {
   res.sendFile(__dirname + "/src/HTML/signin.html");
 });
 
-app.post("signin_process", function (req, res) {
+app.post("/signin", function (req, res) {
+  const id = req.body.id;
+  const password = req.body.password;
 
+  let users = [];
+  try {
+    const data = fs.readFileSync("userinfo.json");
+    users = JSON.parse(data.toString());
+    if (users.find(users => users.uid === id && users.pw === password)) {
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      res.write(`<script>alert('${id} is succeed')</script>`);
+      res.end("<script>window.location='/'</script>");
+    }
+    else {
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      res.write(`<script>alert('${id} is failed')</script>`);
+      res.end("<script>window.location='/signin'</script>");
+    }
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 });
+
 // 회원가입 창
 app.get("/signup", function (req, res) {
   res.sendFile(__dirname + "/src/HTML/signup.html");
 });
 
 // 회원가입
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const name = req.body.name;
   const id = req.body.id;
   const password = req.body.password;
